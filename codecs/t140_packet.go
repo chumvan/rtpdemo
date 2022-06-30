39aaa087384465e801eaa5e72cd9353c2bfb61dd
Trung Van <trung.van@ericsson.com>
2022-06-30 16:12:27 +0300

    structure TODO

--- codecs/t140_packet.go
+++ codecs/t140_packet.go
@@ -1,1 +1,28 @@
 package codecs
+
+// a text/t140 RTP packet without redundancy
+//
+// 0                   1                   2                   3
+// 0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1
+// +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+// |V=2|P|X| CC=0  |M|   T140 PT   |       sequence number         |
+// +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+// |                      timestamp (1000Hz)                       |
+// +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+// |           synchronization source (SSRC) identifier            |
+// +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+// |                      T.140 encoded data                       |
+// +                                               +---------------+
+// +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+
+const (
+	t140HeaderSize = 12 // from V to SSRC
+	t140BlockSize  = 7  // ≈ T.140 encoded data
+
+)
+
+// TODO T140 Header Implementation
+
+// TODO T140 Payload Implementation
+
+// TODO T140 Packet Implementation
