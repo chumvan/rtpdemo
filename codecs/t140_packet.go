c7d90dcde1b92751a99cdbcfdfea309b6647a0fb
Trung Van <trung.van@ericsson.com>
2022-07-01 08:17:08 +0300

    add guideline for packet with redundancy

--- codecs/t140_packet.go
+++ codecs/t140_packet.go
@@ -2,31 +2,52 @@ package codecs
 
 // Follow standard: https://datatracker.ietf.org/doc/html/rfc4103
 
-// a text/t140 RTP packet without redundancy
+// A text/t140 RTP packet without redundancy RFC4103-section7.7.1
 //
 // 0                   1                   2                   3
 // 0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1
-// +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+ -----------------------------
+// +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+ 	-----------------------------
 // |V=2|P|X| CC=0  |M|   T140 PT   |       sequence number         |
 // +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
-// |                      timestamp (1000Hz)                       |		Header
+// |                      timestamp (1000Hz)                       |		Header = 12 bytes
 // +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
 // |           synchronization source (SSRC) identifier            |
-// +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+ ------------------------------
-// |                      T.140 encoded data                       |		Payload
+// +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+ 	------------------------------
+// |                      T.140 encoded data                       |		Payload = 7 bytes
 // +                                               +---------------+
-// +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+				 ------------------------------
+// +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+				 	------------------------------
+
+// A text/t140 RTP packet with one redundant T140block RFC4103-section7.7.1
+// 0                   1                   2                   3
+//     0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1
+//    +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+//    |V=2|P|X| CC=0  |M|  "RED" PT   |   sequence number of primary  |
+//    +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+//    |               timestamp of primary encoding "P"               |
+//    +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+//    |           synchronization source (SSRC) identifier            |
+//    +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+//    |1|   T140 PT   |  timestamp offset of "R"  | "R" block length  |
+//    +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+//    |0|   T140 PT   | "R" T.140 encoded redundant data              |
+//    +-+-+-+-+-+-+-+-+                               +---------------+
+//    +                                               |               |
+//    +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+     +-+-+-+-+-+
+//    |                "P" T.140 encoded primary data       |
+//    +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
 
 const (
 	t140HeaderSize = 12 // from V to SSRC
-	t140BlockSize  = 7  // ≈ T.140 encoded data size
 
 )
 
 //
 // TODO T140 Header Implementation
-//
-
+//	- [x] framing
+//	- [ ] newT140Header()
+//	- [ ] Unmarshal()
+//  - [ ] Marshal()
+//	- [ ] Clone()
 type T140Header struct {
 	Version        uint8
 	Padding        bool
@@ -42,8 +63,13 @@ type T140Header struct {
 //
 // TODO T140 Packet Implementation
 //
+// - [ ] framing
+// - [ ] String()
+// - [ ] Unmarshal()
+// - [ ] Marshal()
+// - [ ] Clone()
 
 type T140Packet struct {
-	T140Header
+	Header  T140Header
 	Payload []byte
 }
